a08f4e98faf96f8fbdbd86d9c24c8717a14cb652
mathem25 <149500529+mathem25@users.noreply.github.com>
2023-12-11 16:31:23 -0500

    Add files via upload

---
+++ Printer Uno.py
@@ -0,0 +1,136 @@
+import random
+
+def printer():
+    #str("indent")= "\t\t\t"
+    for static in no(scanner, finish):
+        clear()
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        print(color.str("reset")+"indent"+"\b\b\b\b\b\b\b"+"please use(control,-) and (control,+) to fit the margin into your laptop")
+        print("<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>")
+        doneEnter(done)
+
+def static(printer):
+    clear()
+    print("\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("indent"+"\t           "+"reverse_emoji<@●●●●●●●.Uno Game.●●●●●●●@>skip_emoji")
+    print("\n\n")
+    print("indent"+"\t           "+"                    1. New game")
+    print("\n")
+    print("indent"+"\t           "+"                       2. exit")
+    print("indent"+"\t             "+"skip_emoji<@●●●●●●●●●●●●●●●●●●●●●●●●●@>reverse_emoji")
+    print("\n\n")
+    print("indent" + "\t           "+"                      0_0?")
+
+def static (num_player):
+    clear()
+    print("\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("indent" + "\b\b\b" + "Enter the number of players(0 < n < 9):")
+
+def static(playerName):
+    clear()
+    print("\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("indent"+ "\b"+ "Hello" + color.str("Blue_Light_L")+player_name+ color.str("reset")+ "Select a password for yourself:")
+
+def static(player_turn):
+    clear()
+    print("\n\n\n\n\n\n\n\n\n\n\n\n")
+    print(color.str("reset")+"\t\t  "+ "Hi"+ color.str("Blue_Light_L")+player.firstName()+color.str("reset")+"It is your turn and please enter your pass to proceed:")
+
+def static(player_chosen_color):
+    print("Use the wild card,then choose a color."+color.str("Red")
+          +color.str("Blue_L")+"1"+color.str("reset")+","+color.str("Red")+
+          color.str("Green_L")+"2"+color.str("reset")+","+color.str("Red")+
+          color.str("Yellow_L")+"3"+color.str("reset")+","+color.str("Red")+
+          color.str("Purple_L")+"4"+color.str("reset")+")")
+    
+def static(gameBoard):
+    clear()
+    while(j=="1", j<="7","j++"):
+        print("indent"+"\t\t\b"+ cardBoard.str(-j)+"  "+cardBoard.str(j))
+        if (j=="2"):
+            print("table color")
+        elif j > 2 and j < 6:
+            print("  "+color.str(cardBoard)+ "  "+color.str("reset"))
+            print("\n")
+            print("\n\n\n")
+
+def static(num_playersCard):
+                  print("num_playersCard:\n")
+                  int(center == 0)
+
+def player(players):
+    print("\t"+player_firstName+":  "+ player.num_playersCard)
+    if (center == 0 and currentPlayer == player_size(-1)):
+        print("\t---> next_player")
+        if (center == currentPlayerIndex):
+            print("\t---> currentPlayer")
+    elif (center-1 == currentPlayerIndex):
+        print("\t---> next_player")
+        print("\n")
+        print("\n\n")
+
+def static (playerCard):
+    while(j == 0,j < playerCard.size(), j == 9):
+        while (i == 1, i <= 8):
+            print("\t\b")
+        if int(k)== j:
+            print(playerCard.str(k))
+        if k < j+9:
+            print(playerCard.str(i))
+        if i == 8:
+            space = playerCard.str(k)-playerCard.str(len(i-1))- playerCard(k).str(len(i))
+            print("")
+            print("\n")
+
+def static (playerChoice):
+    print("n\hey")
+    print(color.str("Blue_Light_L"))
+    print(player_firstName)
+    print(color.str("reset"))
+    print("please pick a card")
+    print("type in the code of the chosen card:")
+
+def static(scores):
+    clear()
+    print(color.str("Red")+ "\n\n\n\n\n\n\n")
+    print("indent"+ "\b\b\b\b\b\b"+ "player_name|player_score"+ "num_playerCard")
+    print("indent"+ "\b\b\b\b\b\b----------------------------------------------")
+
+def static(index = 0):
+     for index in player.size(""):
+         while(currentPlayer == player(index)):
+             print("%s\b\b\b\b\b\b%13s")
+             print("%7d")
+             print("%8d\n")
+             print("indent")
+             currentPlayer.firstName()
+             currentPlayer.score()
+             current.num_playerCard()
+             print("\n\n\n\n")
+             print(color.str("reset"))
+             print(finish)
+
+def static(finish):
+    print("indent")
+    print("\t")
+    print(color.str("Green"))
+    print(color.str("Purple"))
+    print("<@! the input is not accessible!@>")
+    print(color.str("reset"))
+    print(finish)
+
+def static(source):
+    print("indent"+"\t\t"+("type enter to proceed"))
+    source.nextLine()
+
+def static(clear):
+          print("\066[H\066[32J")
+          
+        
+    
+        
+        
+                  
+                  
+            
+        
