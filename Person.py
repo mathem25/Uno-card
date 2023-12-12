fa9e33ac578d9d55c0f9a14cf3b9dea3e98c34b0
mathem25 <149500529+mathem25@users.noreply.github.com>
2023-12-11 22:14:03 -0500

    Add files via upload

---
+++ Person.py
@@ -0,0 +1,35 @@
+import random
+
+def person():
+    for firstName in person:
+        return str(firstName)
+        for lastName in person:
+            return str(lastName)
+            for age in person:
+                return str(age)
+                for gender in person:
+                    return str(gender)
+
+def setfirstName(firstName):
+    this.firstName = firstName
+def setlastName(lastName):
+    this.lastName = lastName
+def setAge(age):
+    this.age= age
+def setGender(gender):
+    this.gender= gender
+def firstName():
+    return firstName
+def lastName():
+    return lastName
+def getAge():
+    return age
+def getGender():
+    return gender
+def string():
+    return "Person first name:"+ firstName+","+"Person last name:"+ lastName+","+"Person age:"+ age+","+"Person gender:"+gender + ".\n"
+def print():
+    print(this.string())
+    
+
+                    
