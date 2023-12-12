1e69be993a09bcf3064f399b4641ea7900a7f8d1
mathem25 <149500529+mathem25@users.noreply.github.com>
2023-12-11 21:35:07 -0500

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,51 @@
+import random
+
+def run (python):
+    for(static)in newscanner(system):
+        for static in str(arguments):
+            int(printer.calibrate)
+            str(int(hold))
+            int(num_players)
+            str(newPlayerName,newPlayerPass)
+            while("player choose exit"):
+                return True
+            while("player choose valid option"):
+                return True
+            printer.printMenu()
+            int(hold)== int(nextLine())
+            while(len.char(hold(0)) == "1",char(hold(0)== "2")):
+                  return
+            else:
+                printer.invalid(int(error))
+                switch= int(hold)
+                for case in "1":
+                    while (Choosenplayer==int(valid)):
+                            return True
+                            printer.num_players()
+                            int(hold)== int(nextLine())
+                            while(len.hold()== 1, char.hold(0)> "0", char.hold(0)<"8"):
+                                       return True
+                            else:
+                                 printer.invalid(int(error))
+                                 num_player = int(hold).char(0)- int("0")
+                                 while (n == 0, n <num_players):
+                                                              printer.playerName(n+1)
+                                                              playerName= int(nextLine())
+                                                              if (playerName.lowerCase() == "bot"):
+                                                                  rules.addPlayer(bot(n))
+                                                              else:
+                                                                   printer.playerPass(PlayerName)
+                                                                   playerPass = int(nextLine())
+                                                                   rules.addPlayer(newPlayer(newPlayerName,newPlayerPass))
+                                                                   rules.preparationGameCards()
+                                                                   rules.distributeCards()
+                                                                   rules.int(runGame)
+                                                                   rules.reset()
+                                                                   for case in "2":
+                                                                                   return True
+                                                                    
+                                                                        
+                                                            
+                                                                
+            
+    
