10ba13f7b752c8b8100f075b08e4ba15232fa661
mathem25 <149500529+mathem25@users.noreply.github.com>
2023-12-11 21:57:31 -0500

    Add files via upload

---
+++ Player.py
@@ -0,0 +1,40 @@
+import random
+
+def player(person):
+    int(score)== player_score
+    if(playerPass):
+        return list_playerCards
+
+def player(firstName, playerPass):
+    super(firstName,"Manu","0","5678Cfcx@$")
+    score= 0
+    playerPass = playerPass
+    playerCards = list_card()
+
+def playerscore():
+    return score
+def playerPass():
+    return playerPass
+def playerCards():
+    return playerCards
+def num_playerCards():
+    return playerCards.size()
+def addCard(card):
+    score += addCard.cardScore()
+    playerCards.add(addCard)
+def removeCard(cardCode):
+    null = removeCard
+    for card in playerCards:
+        if (cardCode() == removecardCode):
+            removeCard = card
+            return
+        score -= removeCard.cardScore()
+        removeplayerCards(removeCards)
+        return removeCard
+
+def boolean(cardCode):
+    for card in playerCards:
+        if (cardCode == card.getCardcode()):
+            return True
+        return False
+    
