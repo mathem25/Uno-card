de3bd7d9381fdd5dc12952ab6b5d1924154f8a34
mathem25 <149500529+mathem25@users.noreply.github.com>
2023-12-11 22:52:30 -0500

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,38 @@
+import random
+
+def bot(extendPlayer):
+    def bot(num):
+        super("Bot"+num,""+num)
+
+def playerCardTurn(player,penaltyCards,botindex):
+    null = botChoosenCard
+    while(n==0, n<super.playerCards().size()):
+        botChoosenCard = super.playerCards(n)
+        if(rules.checkChoose(botChosenCard,this)):
+            this.playerCards().remove(botChoosenCard)
+            score -= botChoosenCard.cardScore()
+            return
+        if (botChoosenCard == wildCard and botChoosenCard == drawWildCard):
+            random.rand == random()
+            switch(rand.int(4)+1)
+            for case in "1":
+                rules.applyChoose(botChoosenCard, "Blue")
+                return
+            for case in "2":
+                rules.applyChoose(botChoosenCard, "Green")
+                return
+            for case in "3":
+                rules.applyChoose(botChoosenCard, "Yellow")
+                return
+            for case in "4":
+                rules.applyChoose(botChoosenCard, "Red")
+                return
+            else:
+                rules.applyChoose(botChoosenCard,botChoosenCard.cardColor())
+            if(botChoosenCard == wildDrawCard):
+                int(index)==(bot_index+1)%players.size()
+                int(n) == penaltyCards.size()
+                while(n>0):
+                    players(index)+penaltyCards(0)== penaltyCards.remove(0)
+            return botChoosenCard
+                    
