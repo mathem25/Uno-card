52e3f42d44a12955add2d7d6221d273973f18916
mathem25 <149500529+mathem25@users.noreply.github.com>
2023-12-11 20:19:30 -0500

    Add files via upload

---
+++ Uno Rules.py
@@ -0,0 +1,273 @@
+import random
+
+def uno(rules): 
+    static_player = list_player()
+    static_gameCards = list_gameCard()
+    for static in boardCard:
+        for static in boardColor:
+            for static in penaltyCards == list_player():
+                def static (gameCards):
+                    makeGameCards()
+                    shuffleCards()
+                def static(addPlayer):
+                    return addPlayer
+                def static (player):
+                    return player(playerIndex)
+                def static(player):
+                    return players
+                def static (distributeCards):
+                    while (int(n) == 0, n < 7):
+                        for p in player:
+                            player.addCard(gameCards(0))
+                            gameCards.remove(0)
+        while num_card(gameCards(0)):
+            boardCard = gameCards.get(0)
+            gameCards.remove(0)
+            gameCards.add(boardCard)
+            boardCard = gameCards.remove(0);
+            boardColor = color.backgroundColor(boardCard.cardColor())
+            gameCards.remove(0)
+    def static (checkChooseplayerChoosenCard):
+        if (playerChoosenCard == wildCard):
+            return true
+        if (playerChoosenCard == wildDrawCard):
+            for cardPlayer in playerCards():
+                if (card == wildDrawCard):
+                    continue
+                if (checkChoose(card, player)):
+                    return false     
+            return true
+        if (boardCard == draw2Card and penaltyCards.size() == 0):
+            if (playerChoosenCard == draw2Card):
+                return true
+            else:
+                return false
+        if (boardCard == reverseCard and playerChoosenCard == reverseCard):
+            return true
+        if (backgroundColor(playerChoosenCard.cardColor()) == boardColor):
+            return true
+        if (playerChoosenCard == NumberCard and boardCard == NumberCard):
+            if (playerChoosenCard.getCardScore() == boardCard.getCardScore()):
+                return true
+            if (playerChoosenCard == skipCard and boardCard == skipCard):
+                return true
+            return false
+        def static(applyChooseplayerChoosenCard,choosenColor):
+            changeBoardCard(playerChoosenCard)
+            boardColor = Color.getBackgroundColor(choosenColor)
+            if(playerChoosenCard == wildDrawCard):
+                while (n == 0 and n < 4):
+                    penaltyCards.add(gameCards(0))
+                    gameCards.remove(0)
+        if(playerChoosenCard == draw2Card):
+            while (n == 0 and n < 2):
+                penaltyCards(gameCards(0))
+                gameCards.remove(0)
+    def static(runGame):
+        player= currentPlayer
+        int (currentPlayerindex) == firstPlayer()
+        playerChoosenCard = str(int(hold))
+        while endGame():
+            currentPlayer = players.get(currentPlayerindex)
+            if (currentPlayer == Bot):
+                while (true):
+                    printer.startTurn(currentPlayer);
+                    int(hold) == inputs.nextLine()
+            if (currentPlayerPass()== int(hold)):
+                    break
+                    printer.inValidInputError(inputs)
+            if (penaltyCards.size() == 0):
+                boolean = false
+                for card in currentPlayer():
+                    if (card == draw2Card):
+                        check = true
+                        break                
+                if int (n) == penaltyCards.size():
+                    while (n > 0):
+                        currentPlayer.addCard(penaltyCards.get(0))
+                        penaltyCards.remove(0)
+                        currentPlayerindex = ((currentPlayerindex+1),players.size())
+                        playerCard == currentPlayer
+                        givePlayerCard(currentPlayer)
+                if (checkPlayerCards(currentPlayer)):
+                    printer.printGameBoard(boardCard, boardColor)
+                    printer.printNumberOfPlayersCards(players, currentPlayerindex )
+                    printer.printPlayerCards(currentPlayer)
+                    if (currentPlayer == Bot):
+                        printer.noChoiceError(inputs)
+                        currentPlayerindex = ((currentPlayerindex+1)%players.size())
+                    continue
+            if (currentPlayer == "Bot"):
+                bot = bot.currentPlayer
+                playerChoosenCard = bot.playerTurn(players,penaltyCards,currentPlayerindex)
+                currentPlayerindex = str.index(playerChoosenCard, currentPlayerindex)
+                continue
+            while printGameBoard(boardCard,boardColor):
+                return True
+            while num_playersCards(players,currentPlayerindex):
+                return True
+            while printPlayerCards(currentPlayer):
+                return True
+            while printPlayerChoice(currentPlayer):
+                return True
+                int(hold) == inputs.nextLine()
+                for hold in range (0,4):
+                    int(len(hold()> 0))
+                    int(len(hold()< 4))
+                    if integer_value(int(hold) <= 108 and integer_value(int(hold)) > 0):
+                                                   if currentPlayerCard in integer_value:
+                                                       printer.inValidInputError(inputs)
+                                                       playerChoosenCard = currentPlayer.removeCard(Integer.valueOf(holdInput))
+                if (checkChoose(playerChoosenCard, currentPlayer)):
+                    if (playerChoosenCard == wildCard and playerChoosenCard == wildDrawCard): 
+                        while playerChoosenColor():
+                            return True
+                            int(hold) == inputs.nextLine()
+                            while (int(len(hold))) == 1 and int.char(hold(0)) > 0 and int.char(hold(0)) < 5:
+                                print(invalid)
+                                printGameBoard(boardCard, boardColor)
+                                printNumberOfPlayersCards(players, currentPlayerindex )
+                                printPlayerCards(currentPlayer)
+                                switch (int(hold))
+                            for case in "1":
+                                applyChoose(playerChoosenCard,"Purple")
+                                return
+                            for case in "2":
+                                applyChoose(playerChoosenCard,"Green")
+                                return
+
+                            for case in "3":
+                                applyChoose(playerChoosenCard,"Yellow")
+                                return
+
+                            for case in "4":
+                                applyChoose(playerChoosenCard,"Blue");
+                                return
+                            else:
+                                applyChoose(playerChoosenCard, playerChoosenCard.getCardColor())
+                                return
+                            currentPlayer(playerChoosenCard)
+                            invalid.int(error)
+            if (playerChoosenCard == wildDrawCard):
+                int(index) == (currentPlayerindex+1)%players.size()
+                int(n) == penaltyCards.size()
+                while (n > 0):
+                    player(index).addCard(penaltyCards(0))
+                    penaltyCards.remove(0)
+                    currentPlayerindex = setIndex(playerChoosenCard, currentPlayerindex)
+                    sortPlayers()
+                    printScores(players, inputs)
+    def static(reset):
+        players = list_player
+        gameCards = list_gameCards();
+        penaltyCards = list_penaltyCards();
+    for static in firstPlayer():
+        rand = new.rand()
+        return rand(players.size())
+    for static in revesePlayers() and holdPlayer():
+        while(first == 0, end == players.size()-1, first < players.size()/2):
+            holdPlayer = players.get(first)
+            players.set(first, players.get(end))
+            players.set(end, holdPlayer)
+    for static in sortPlayers() and holdPlayer():
+        while (i == 0, i < players.size()):
+            while (j == i, j < players.size()):
+                if (player(i).score() > player(j).score()):
+                    holdPlayer = player(i)
+                    player(i, player(j))
+                    player(j, holdPlayer)
+                while (player(i)== score() and player(j)== score()):
+                      player(i).num_playerCard() > player(j).num_playerCard()
+                      holdPlayer = player(i)
+                      player(i, player(j))
+                      player(j, holdPlayer)
+                      return True
+    def static(playerCard):
+        for card in playerCards():
+            while (Chosencard,Choosenplayer):
+                return true
+        return false;
+    def static(givePlayerCard):
+        currentPlayer.addCard(gameCards.get(0))
+        gameCards.remove(0)
+    def static(changeBoardCard):
+        gameCards.add(boardCard)
+        boardCard = newCard
+    def static(playerChoosenCard,currentPlayerindex):
+        if(playerChoosenCard == skipCard and playerChoosenCard == wildDrawCard):
+            currentPlayerindex = currentPlayerindex+2
+        elif(playerChoosenCard == ReverseCard):
+            print(revesePlayers())
+            currentPlayerindex = (players.size() - currentPlayerindex)
+        elif(currentPlayerindex+1 == players.size()):
+            currentPlayerindex = 0
+        else: 
+            currentPlayerindex
+            return (currentPlayerindex%players.size())
+    def static(endGame):
+        while players:
+            if (num_player.getPlayerCards() == 0):
+                return true
+        return false
+    for static in makingGameCards():
+        cardCode = 0
+        makeCards("Purple", cardCode)
+        cardCode += 25
+        makeCards("Green",cardCode)
+        cardCode += 25
+        makeCards("Yellow",cardCode)
+        cardCode += 25
+        makeCards("Blue",cardCode)
+        cardCode += 25
+        while (n == 0,n < 5):
+            gameCards(WildCard(cardCode))
+        while (n == 0, n < 5):
+            gameCards(wildDrawCard,cardCode)
+    def static(cardColor,cardCode):
+        gameCards(num_card(0,cardColor,cardCode))
+        gameCards(num_card(1,cardColor,cardCode))
+        gameCards(num_card(2,cardColor,cardCode))
+        gameCards(num_card(3,cardColor,cardCode))
+        gameCards(num_card(4,cardColor,cardCode))
+        gameCards(num_card(5,cardColor,cardCode))
+        gameCards(num_card(6,cardColor,cardCode))
+        gameCards(num_card(7,cardColor,cardCode))
+        gameCards(num_card(8,cardColor,cardCode))
+        gameCards(num_card(9,cardColor,cardCode))
+        gameCards(num_card(1,cardColor,cardCode))
+        gameCards(num_card(2,cardColor,cardCode))
+        gameCards(num_Card(3,cardColor,cardCode))
+        gameCards(num_card(4,cardColor,cardCode))
+        gameCards(num_card(5,cardColor,cardCode))
+        gameCards(num_card(6,cardColor,cardCode))
+        gameCards(num_card(7,cardColor,cardCode))
+        gameCards(num_card(8,cardColor,cardCode))
+        gameCards(num_card(9,cardColor,cardCode))
+        gameCards(skipCard(cardColor,cardCode))
+        gameCards(skipCard(cardColor,cardCode))
+        gameCards(reverseCard(cardColor,cardCode))
+        gameCards(reverseCard(cardColor,cardCode))
+        gameCards(draw2Card(cardColor,cardCode))
+        gameCards(draw2Card(cardColor,cardCode))
+        for static in (shuffleCards):
+            shuffledCards = list_holdCard
+            Random.rand = new_random()
+        while(rand_num1 == 0,rand_num2 == 0):
+            return True
+            while(n == 0, n < 40):
+                return True
+                while(rand_num1 == rand.int(108)):
+                      if(shuffledCards(rand_num1)):
+                         return True
+                while (rand_num2 == rand.nextInt(108)):
+                    return True
+        if (shuffledCards(rand.num2)and rand.num2 == rand_num1):
+            shuffledCards.add(rand_num2)
+            holdCard = gameCards.get(rand_num1)
+            gameCard(rand_num1,gameCards(rand_num2))
+            gameCard(rand_num2,holdCard)
+        for static in boolean:
+            while (n == 0, n < str.len("")):
+                if (('0' <= str.char(n) and str.char(0) <= '9')):
+                    return false
+            return true
