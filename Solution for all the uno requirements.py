ab0d1304548f77dbb53ae7d28da54169e685e8e3
mathem25 <149500529+mathem25@users.noreply.github.com>
2023-12-11 23:27:37 -0500

    Add files via upload

---
+++ Solution for all the uno requirements.py
@@ -0,0 +1,30 @@
+Python 3.11.6 (tags/v3.11.6:8b6ee5b, Oct  2 2023, 14:57:12) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================= RESTART: D:/Final Project/Bot.py =======================
+>>> 
+==================== RESTART: D:\Final Project\Draw2Card.py ====================
+>>> 
+=================== RESTART: D:\Final Project\Number card.py ===================
+>>> 
+====================== RESTART: D:\Final Project\Person.py =====================
+>>> 
+====================== RESTART: D:\Final Project\Player.py =====================
+>>> 
+=================== RESTART: D:\Final Project\Printer Uno.py ===================
+>>> 
+=================== RESTART: D:\Final Project\Reverse Card.py ==================
+>>> 
+======================= RESTART: D:\Final Project\Run.py =======================
+>>> 
+==================== RESTART: D:\Final Project\Skip card.py ====================
+>>> 
+===================== RESTART: D:\Final Project\Uno Card.py ====================
+>>> 
+==================== RESTART: D:\Final Project\Uno Color.py ====================
+>>> 
+==================== RESTART: D:\Final Project\Uno Rules.py ====================
+>>> 
+==================== RESTART: D:\Final Project\Wild card.py ====================
+>>> 
+================== RESTART: D:\Final Project\Wild draw card.py =================
